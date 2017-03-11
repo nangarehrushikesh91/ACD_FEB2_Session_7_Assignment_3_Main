4444bcf6b1aa7ecaadcb888fc2104627231f013e
nangarehrushikesh91 <nangarehrushikesh91@gmail.com>
2017-03-11 19:11:39 +0530

    Submission of assignments
    Submission of assignments

---
+++ app.js
@@ -0,0 +1,22 @@
+var i = 0;
+
+function counter(n){
+
+  n++;
+  return n;
+}
+
+i = counter(i); // Callinf function first time
+console.log(i);
+
+i = counter(i); // Callinf function second time
+console.log(i);
+
+i = counter(i); // Callinf function third time
+console.log(i);
+
+i = counter(i); // Callinf function fourth time
+console.log(i);
+
+i = counter(i); // Callinf function fifth time
+console.log(i);
